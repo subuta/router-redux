71e1cc7e161bc5119dfa3b3cc7ed4df8b6e3ec20
shunta saito <s.shunta@goodpatch.com>
2016-08-02 15:11:27 +0900

    [modify]remove not necessary files.

--- example/components/Routes.js
+++ example/components/Routes.js
@@ -5,26 +5,19 @@ import {
 } from 'example/store.js'
 
 import {
-  getCurrent,
-  getLast,
-  isEnter,
-  isLeave
+  getCurrent
 } from '/lib/index.js';
 
 export default inject(({state}) => {
   const current = getCurrent(state);
-  const last = getLast(state);
-  const _isEnter = isEnter(state, '/test');
-  const _isLeave = isLeave(state, '/test');
   console.log("current = ", current);
-  console.log("last = ", last);
-  console.log("isEnter to test = ", _isEnter);
-  console.log("_isLeave from test = ", _isLeave);
   if (current === '/') {
     return h('h1', {}, 'top');
   } else if (current === '/test') {
     return h('h1', {}, 'test');
   } else if (current === '/foo') {
     return h('h1', {}, 'bar');
+  } else {
+    return h('h1', {}, 'not found...');
   }
 });
