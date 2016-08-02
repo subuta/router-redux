076bce9144eab9150607337eb2ad86fc76a4fb6e
shunta saito <s.shunta@goodpatch.com>
2016-08-02 14:39:13 +0900

    [modify]add working changes.

--- example/components/Routes.js
+++ example/components/Routes.js
@@ -4,13 +4,27 @@ import {
   inject
 } from 'example/store.js'
 
+import {
+  getCurrent,
+  getLast,
+  isEnter,
+  isLeave
+} from '/lib/index.js';
+
 export default inject(({state}) => {
-  const {pathname} = state.routing.current;
-  if (pathname === '/') {
+  const current = getCurrent(state);
+  const last = getLast(state);
+  const _isEnter = isEnter(state, '/test');
+  const _isLeave = isLeave(state, '/test');
+  console.log("current = ", current);
+  console.log("last = ", last);
+  console.log("isEnter to test = ", _isEnter);
+  console.log("_isLeave from test = ", _isLeave);
+  if (current === '/') {
     return h('h1', {}, 'top');
-  } else if (pathname === '/test') {
+  } else if (current === '/test') {
     return h('h1', {}, 'test');
-  } else if (pathname === '/foo') {
+  } else if (current === '/foo') {
     return h('h1', {}, 'bar');
   }
 });
--- example/store.js
+++ example/store.js
@@ -1,6 +1,6 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import injectCreator from 'redux-virtual-dom';
-import routerCreator, {routerMiddlewareCreator} from 'lib/index.js';
+import routerListenerCreator, {routerMiddlewareCreator} from 'lib/index.js';
 
 import reducer from './reducers/index.js';
 const middlewares = [routerMiddlewareCreator()];
@@ -12,6 +12,14 @@ const store = createStore(reducer, compose(
 
 // create inject for your store.
 export const {inject, connect} = injectCreator(store);
-export const router = routerCreator(store);
+export const listener = routerListenerCreator(store);
+
+// router.onEnter('/test', () => {
+//   console.log('enter in test route!');
+// });
+//
+// router.onLeave('/test', () => {
+//   console.log('leave from test route...');
+// });
 
 export default store;
