02df0a5970e4d2030ab42d5ca5e1829eeb9ae2ba
shunta saito <s.shunta@goodpatch.com>
2016-08-02 23:27:04 +0900

    [modify]add onEnter/onLeave hook.

--- spec/middleware.spec.js
+++ spec/middleware.spec.js
@@ -6,14 +6,27 @@ import {
   FORWARD
 } from 'lib/actions.js';
 import middlewareCreator from 'lib/middleware.js';
+import {transformToPath} from 'lib/reducer.js';
+import routerCreator from 'lib/router.js';
 
 describe('middleware', function() {
 
   let middleware;
   let dispatch;
   let sandbox;
+  let router;
   beforeEach(function(){
     sandbox = sinon.sandbox.create();
+    const store = {
+      getState: sandbox.spy(() => {
+        return {
+          routing: {
+            current: transformToPath(location)
+          }
+        }
+      }),
+      dispatch: sandbox.spy(() => {})
+    };
 
     // starts with '/'
     history.pushState(null, null, '/');
@@ -25,8 +38,9 @@ describe('middleware', function() {
     history.back = sandbox.spy(history, 'back');
     history.forward = sandbox.spy(history, 'forward');
 
+    router = routerCreator(store);
     dispatch = sandbox.spy((action) => action);
-    middleware = middlewareCreator(history)({})(dispatch);
+    middleware = middlewareCreator(history)(store)(dispatch);
   });
 
   afterEach(function(){
@@ -34,76 +48,158 @@ describe('middleware', function() {
   });
 
   it('should accept push action', function(){
-    assert.deepEqual(middleware({
+    middleware({
       type: PUSH,
-      payload: '/test'
-    }), {
+      payload: '/sample'
+    });
+
+    assert.equal(dispatch.called, true);
+    assert.equal(dispatch.calledWith({
       type: PUSH,
       payload: location
-    });
-    assert.equal(dispatch.called, true);
+    }), true);
     assert.equal(history.pushState.called, true);
   });
 
   it('should not accept duplicated push action', function(){
     // try to change url from '/' to '/'
-    assert.deepEqual(middleware({
+    middleware({
       type: PUSH,
       payload: '/'
-    }), undefined);
+    });
+
     assert.equal(dispatch.called, false);
     assert.equal(history.pushState.called, false);
   });
 
   it('should accept replace action', function(){
-    assert.deepEqual(middleware({
+    middleware({
       type: REPLACE,
-      payload: '/test'
-    }), {
+      payload: '/sample'
+    });
+
+    assert.equal(dispatch.called, true);
+    assert.equal(dispatch.calledWith({
       type: REPLACE,
       payload: location
-    });
-    assert.equal(dispatch.called, true);
+    }), true);
     assert.equal(history.replaceState.called, true);
   });
 
   it('should accept go action', function(){
-    assert.deepEqual(middleware({
+    middleware({
       type: GO,
       payload: -1
-    }), {
+    });
+
+    assert.equal(dispatch.called, true);
+    assert.equal(dispatch.calledWith({
       type: GO,
       payload: location
-    });
-    assert.equal(dispatch.called, true);
+    }), true);
     assert.equal(history.go.called, true);
   });
 
   it('should accept back action', function(){
-    assert.deepEqual(middleware({
+    middleware({
       type: BACK
-    }), {
+    });
+
+    assert.equal(dispatch.called, true);
+    assert.equal(dispatch.calledWith({
       type: BACK,
       payload: location
-    });
-    assert.equal(dispatch.called, true);
+    }), true);
     assert.equal(history.back.called, true);
   });
 
   it('should accept forward action', function(){
-    assert.deepEqual(middleware({
+    middleware({
       type: FORWARD
-    }), {
+    });
+
+    assert.equal(dispatch.called, true);
+    assert.equal(dispatch.calledWith({
       type: FORWARD,
       payload: location
-    });
-    assert.equal(dispatch.called, true);
+    }), true);
     assert.equal(history.forward.called, true);
   });
 
   it('should pass-through other action', function(){
-    assert.deepEqual(middleware({}), {});
+    middleware({});
+
+    assert.equal(dispatch.calledWith({}), true);
     assert.equal(dispatch.called, true);
+  });
+
+  it('should call router\'s onEnter hook on push', function(){
+    const onEnter = sinon.spy((state, cb) => {
+      assert.deepEqual(state, {
+        routing: {
+          current: transformToPath(location)
+        }
+      });
+      cb();
+    });
+    router.onEnter('/sample', onEnter);
+
+    middleware({
+      type: PUSH,
+      payload: '/sample'
+    });
+
+    assert.equal(dispatch.called, true);
+    assert.equal(dispatch.calledWith({
+      type: PUSH,
+      payload: location
+    }), true);
+    assert.equal(onEnter.called, true);
+    assert.equal(location.pathname, '/sample');
+  });
+
+  it('should not dispatch history action if router\'s onEnter not calls callback.', function(){
+    const onEnter = sinon.spy((state) => {
+      assert.deepEqual(state, {
+        routing: {
+          current: transformToPath(location)
+        }
+      });
+    });
+    router.onEnter('/sample', onEnter);
+
+    middleware({
+      type: PUSH,
+      payload: '/sample'
+    });
+
+    assert.equal(dispatch.called, false);
+    assert.equal(onEnter.called, true);
+    assert.equal(location.pathname, '/');
+  });
+
+  it('should call router\'s onLeave hook on push', function(){
+    const onLeave = sinon.spy((state) => {
+      assert.deepEqual(state, {
+        routing: {
+          current: transformToPath(location)
+        }
+      });
+    });
+    router.onLeave('/', onLeave);
+
+    middleware({
+      type: PUSH,
+      payload: '/sample'
+    });
+
+    assert.equal(dispatch.called, true);
+    assert.equal(dispatch.calledWith({
+      type: PUSH,
+      payload: location
+    }), true);
+    assert.equal(onLeave.called, true);
+    assert.equal(location.pathname, '/sample');
   });
 });
 
