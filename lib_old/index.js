227219517f98d9e2031ce3bd2c9d6bee1136710c
shunta saito <s.shunta@goodpatch.com>
2016-11-13 17:16:52 +0100

    [modify]move old files.

---
+++ lib_old/index.js
@@ -0,0 +1,31 @@
+// middleware
+import middleware from './middleware.js'
+export const routerMiddleware = middleware;
+
+// reducer
+import * as reducer from './reducer.js'
+import * as selector from './selector.js'
+export const routerReducer = reducer.default;
+export const {
+  getCurrent,
+  getLast,
+  getNext,
+  getRouteError,
+  getIsInitalRouteResolved
+} = selector;
+
+import _match from './match.js'
+export const match = _match;
+
+// actions
+import * as actions from './actions.js';
+export const {
+  push,
+  replace,
+  go,
+  back,
+  forward
+} = actions;
+
+import routerCreator from './router.js';
+export default routerCreator;
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,35 +1,7 @@
-import routerCreator from 'lib/index.js';
-import {
-  routerMiddleware,
-  routerReducer,
-  getCurrent,
-  getLast,
-  getNext,
-  getRouteError,
-  getIsInitalRouteResolved,
-  push,
-  replace,
-  go,
-  back,
-  forward,
-  match
-} from 'lib/index.js';
+import index from 'lib/index.js';
 
 describe('index', function() {
-  it('should export things correctly', function(){
-    assert.equal(typeof routerCreator === 'function', true);
-    assert.equal(typeof routerMiddleware === 'function', true);
-    assert.equal(typeof routerReducer === 'function', true);
-    assert.equal(typeof getCurrent === 'function', true);
-    assert.equal(typeof getLast === 'function', true);
-    assert.equal(typeof getNext === 'function', true);
-    assert.equal(typeof getRouteError === 'function', true);
-    assert.equal(typeof getIsInitalRouteResolved === 'function', true);
-    assert.equal(typeof push === 'function', true);
-    assert.equal(typeof replace === 'function', true);
-    assert.equal(typeof go === 'function', true);
-    assert.equal(typeof back === 'function', true);
-    assert.equal(typeof forward === 'function', true);
-    assert.equal(typeof match === 'function', true);
+  it('should create routeChange action', function(){
+    assert.deepEqual(index, 'test')
   });
 });
