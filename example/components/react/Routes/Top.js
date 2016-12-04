fcc4101a96015a59099b7f126ec5e85efdd6a6ee
shunta saito <s.shunta@goodpatch.com>
2016-12-04 23:53:29 +0900

    [modify]change api.

--- example/components/react/Routes/Top.js
+++ example/components/react/Routes/Top.js
@@ -7,32 +7,32 @@ import {
 
 import {
   push,
-  getIsInitalRouteResolved,
+  getIsLoading,
   getRouteError
 } from 'router-redux';
 
-router.onError(({state, dispatch}) => {
-  const routeError = getRouteError(state);
-  console.log('routeError.message = ', routeError.message);
-  dispatch(push('/error'));
-});
-
-router.onEnter('/', ({state}, cb) => {
-  console.log('[top]loading ...', state);
-  setTimeout(() => {
-    console.log('[top]timer fired');
-    // cb(new Error('[top]thrown error'));
-    cb();
-  }, 1000);
-});
-
-router.onLeave('/', ({state}) => {
-  console.log('[top]leave');
-});
+// router.onError(({state, dispatch}) => {
+//   const routeError = getRouteError(state);
+//   console.log('routeError.message = ', routeError.message);
+//   dispatch(push('/error'));
+// });
+//
+// router.onEnter('/', ({state}, cb) => {
+//   console.log('[top]loading ...', state);
+//   setTimeout(() => {
+//     console.log('[top]timer fired');
+//     // cb(new Error('[top]thrown error'));
+//     cb();
+//   }, 1000);
+// });
+//
+// router.onLeave('/', ({state}) => {
+//   console.log('[top]leave');
+// });
 
 export default inject(({state}) => {
-  const isInitialRouteResolved = getIsInitalRouteResolved(state);
-  if (!isInitialRouteResolved) {
+  const isLoading = getIsLoading(state);
+  if (!isLoading) {
     return <h1>loading...</h1>;
   }
   return <h1>top</h1>;
--- example/components/react/Routes/index.js
+++ example/components/react/Routes/index.js
@@ -9,17 +9,17 @@ import {
   match
 } from 'router-redux';
 
-// import Top from './Top.js';
-// import Bar from './Bar.js';
+import Top from './Top.js';
+import Bar from './Bar.js';
 import Foo from './Foo.js';
 import NotFound from './NotFound.js';
 import Error from './Error.js';
 
 router.on('*', () => <NotFound/>)
 router.on('/error', () => <Error error="some error occurred"/>)
-// router.on('/', () => <Top/>)
+router.on('/', () => <Top/>)
 router.on('/foo/:id', Foo)
-// router.on('/bar', () => <Bar/>)
+router.on('/bar', () => <Bar/>)
 
 export default () => {
   return router.render();
